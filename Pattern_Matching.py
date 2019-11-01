e956799aa5ac1d780b30b00dfe9363a7e23307cf
dmunglah <55840220+dmunglah@users.noreply.github.com>
2019-11-01 11:26:45 +0000

    Update Pattern_Matching.py
    Fixing pattern matching branch to use  actual pattern matching to complete set assignment.

--- Pattern_Matching.py
+++ Pattern_Matching.py
@@ -1,21 +1,21 @@
+import re 
+
 accessions = ['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp']
 
-print (accessions[1:8])
+pattern = re.compile(r'd')
+pattern = re.compile(r'e')
+pattern = re.compile(r'de')
+pattern = re.compile(r'5')
+pattern = re.compile(r'5')
+pattern = re.compile(r'd'), re.compile(r'e')
+pattern = re.compile(r'xy')
+pattern = re.compile('\d{0,3}')
+pattern = re.compile(r'd,a,r,p')
+pattern = re.compile(r'xye')
 
-print (accessions[-1])
+matches = pattern.finditer(str(accessions))
 
-from operator import itemgetter  
-
-print(*itemgetter(0, 4, 5, 6)(accessions))
-
-print(accessions[4])
-
-print (accessions[3:6], accessions[-1])
-
-print(*itemgetter(0, 1, 5)(accessions))
-
-print (accessions[-3])
-
-print(*itemgetter(0, 3, 4)(accessions)) 
-
-print(*itemgetter(-1,-2)(accessions)) 
+for match in matches:
+    print(match)
+    
+    
