47e640a6b087b814b6f9f384eb960d7ca44d63d6
dmunglah <55840220+dmunglah@users.noreply.github.com>
2019-11-04 14:23:38 +0000

    Pattern_Matching Update
    Using regex to find assigned patterns in accessions.

--- Pattern_Matching.py
+++ Pattern_Matching.py
@@ -1,21 +1,27 @@
-import re 
+import re # importing regular expression
 
-accessions = ['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp']
+accessions = ['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp'] #raw script
 
-pattern = re.compile(r'd')
-pattern = re.compile(r'e')
+#contain the letter d or e
+pattern = re.compile(r'd|e')
+#contain the letters d and e in that order
 pattern = re.compile(r'de')
-pattern = re.compile(r'5')
-pattern = re.compile(r'5')
-pattern = re.compile(r'd'), re.compile(r'e')
-pattern = re.compile(r'xy')
-pattern = re.compile('\d{0,3}')
-pattern = re.compile(r'd,a,r,p')
-pattern = re.compile(r'xye')
-
+# contain the number 5
+pattern = re.findall(r'5')
+#contain the letters d and e in that order with a single letter between them
+pattern = re.compile(r'd|.|e')
+#contain both the letters d and e in any order
+pattern = re.findall(r'de')
+#start with x or y
+pattern = re.compile(r'x|y')
+#start with x or y and end with e
+pattern = re.compile(r'x|y$e')
+#contain three or more digits in a row
+pattern = re.compile(r'/d{3}.')
+#end with d followed by either a, r or p
+pattern = re.compile(r'd$a|r|p')
 matches = pattern.finditer(str(accessions))
+
 
 for match in matches:
     print(match)
-    
-    
