9fca1ef4db023e6bc9e4aacc9a52ee47f4fd9d10
dmunglah <55840220+dmunglah@users.noreply.github.com>
2019-11-06 11:18:32 +0000

    Update on Pattern_Matching
    The code isn't complete but this what I have so far.

--- Pattern_Matching.py
+++ Pattern_Matching.py
@@ -2,26 +2,62 @@ import re # importing regular expression
 
 accessions = ['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp'] #raw script
 
-#contain the letter d or e
-pattern = re.compile(r'd|e')
+#contains the letter d or e
+for elements in accessions:
+    result1 = re.match(r"(e\w+)", accessions)
+
+if result1:
+    print((result1.groups))
 #contain the letters d and e in that order
-pattern = re.compile(r'de')
-# contain the number 5
-pattern = re.findall(r'5')
+result2 = re.match(r"\wde", accessions)
+#contain the number 5
+for element in accessions:
+    result3 = re.match(r"\W5", accessions)
+
+    # See if string starts in digit.
+    m = re.match("5\d", element)
+    if m:
+        print("The elemetn that contains a number is :", element)
 #contain the letters d and e in that order with a single letter between them
-pattern = re.compile(r'd|.|e')
-#contain both the letters d and e in any order
-pattern = re.findall(r'de')
+for elements in accessions:
+    result1 = re.match(r"(d.e\w+)", accessions)
+
+if result1:
+    print((result1.groups))
+# Find all words starting with d or p.
+for element in accessions:
+
+    # See if string starts in digit.
+    m = re.match("^d|p\w", element)
+    if m:
+        print("START:", element)
+
+    # See if string ends in digit.
+    m = re.match("\wd|p$", element)
+    if m:
+        print("  END:", element)
+
 #start with x or y
-pattern = re.compile(r'x|y')
+for element in accessions:
+
+    # See if string starts in digit.
+    m = re.match("^x|y\w", element)
+    if m:
+        print("START:", element)
+
 #start with x or y and end with e
-pattern = re.compile(r'x|y$e')
+for element in accessions:
+
+    # See if string starts in digit.
+    m = re.match("^x|y\w", element)
+    if m:
+        print("START:", element)
+
+    # See if string ends in digit.
+    m = re.match("\we$", element)
+    if m:
+        print("  END:", element)
 #contain three or more digits in a row
-pattern = re.compile(r'/d{3}.')
+result8 = re.match(r".", accessions)
 #end with d followed by either a, r or p
-pattern = re.compile(r'd$a|r|p')
-matches = pattern.finditer(str(accessions))
-
-
-for match in matches:
-    print(match)
+result9 = re.match(r".*", accessions)
