5ed8625cc37b37c204aede747b89889fc36502be
David <davidmunglah1@gmail.com>
2019-11-07 16:26:32 +0000

    Trying a different approach by re.search and then re.match.

--- Pattern_Matching.py
+++ Pattern_Matching.py
@@ -3,59 +3,54 @@ import re # importing regular expression
 accessions = ['xkn59438', 'yhdck2', 'eihd39d9', 'chdsye847', 'hedle3455', 'xjhd53e', '45da', 'de37dp'] #raw script
 
 #contains the letter e
-for elements in accessions:
-    result1 = re.match(r"(e\w+)", accessions)
+for element in accessions:
 
-if result1:
-    print((result1.groups))
+    match = re.search(r'\w+', 'e')
+    if match:
+        print(match.group, accessions)
 #contain the letters d and e in that order
 for elements in accessions:
-    result2 = re.match(r"(de\w+)", accessions)
+    result2 = re.match(r"(\Bde\w+)", accessions)
 
 if result2:
-    print((result2.groups))
+    print(result2.group)
 #contain the number 5
 for element in accessions:
 
-    result9 = re.match(r'(5\d)', accessions)
-    if result9:
-        print(result9.groups)
+    match = re.search(r'\d', '5')
+    if match:
+        print(match.groups, element)
+        
 #contain the letters d and e in that order with a single letter between them
-for elements in accessions:
-    result3 = re.match(r"(d.e\w+)", accessions)
-
-if result1:
-    print((result1.groups))
-# Find all words starting with d or p.
 for element in accessions:
 
-    result4 = re.match("^d|p\w", accessions)
-    if result4:
-        print("START:", element)
+    match = re.search(r'd.e', 'de')
+    if match:
+        print(match.groups, element)
 
 #start with x or y
-for element in accessions:
-
-    result5 = re.match("^x|y\w", accessions)
-    if element == result5:
-        print("START:", accessions)
+for elements in accessions:
+    result5 = re.match(r"(^x|y\w)", accessions)
+    
+    if result5:
+        print("START:", result5.groups)
 
 #start with x or y and end with e
-for element in accessions:
+for elements in accessions:
 
-    result6 = re.match("^x|y\w", accessions)
+    result6 = re.match(r"(^x|y\w)", accessions)
     if result6:
         print (result6.groups)
 #contain three or more digits in a row
-for element in accessions:
+for elements in accessions:
 
-    result7 = re.match(r"/d/d/d/", accessions)
+    result7 = re.match(r"(/d/d/d/)", accessions)
     if result7:
         print(result7.groups)
 #end with d followed by either a, r or p
-for element in accessions:
+for elements in accessions:
 
-    result8 = re.match(r"$da|r|p\w", accessions)
+    result8 = re.match(r"($da|r|p\w)", accessions)
     if result8:
         print(result8.groups)
 
